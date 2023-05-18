de0c0cac14a7d5bbf47d06a54e54dbca0c05a3a1
limdumb <dlaruddls@nate.com>
2023-05-18 22:17:29 +0900

    [Feat] Route 설정

--- App.tsx
+++ App.tsx
@@ -1,18 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import CafeDetailView from "./pages/CafeDetailView";
+import LoginView from "./pages/LoginView";
+import MainView from "./pages/MainView";
+import MyPageView from "./pages/MyPageView";
+import RoleSelectionView from "./pages/RoleSelectionView";
+import SignUpView from "./pages/SignUpView";
+
+export type RootStackParamList = {
+  MainView: undefined;
+  CafeDetailView: undefined;
+  LoginView: undefined;
+  SignUpView: undefined;
+  RoleSelectionView: undefined;
+  MyPageView: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="MainView" component={MainView} />
+        <Stack.Screen name="CafeDetailView" component={CafeDetailView} />
+        <Stack.Screen name="LoginView" component={LoginView} />
+        <Stack.Screen name="SignUpView" component={SignUpView} />
+        <Stack.Screen name="RoleSelectionView" component={RoleSelectionView} />
+        <Stack.Screen name="MyPageView" component={MyPageView} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- pages/MainView.tsx
+++ pages/MainView.tsx
@@ -1,4 +1,18 @@
-export default function MainView(){
+import { StyleSheet, Text, View } from "react-native";
 
-  return(<></>)
+export default function MainView() {
+  return (
+    <View style={styles.container}>
+      <Text>하이용ㅋㅋ</Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
