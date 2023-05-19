d5d323d91996b805939e005263820f6dff59e5d9
limdumb <dlaruddls@nate.com>
2023-05-19 11:41:14 +0900

    [Feat] Cafe Detail View Tab Layout 설정

--- pages/CafeDetailView.tsx
+++ pages/CafeDetailView.tsx
@@ -1,4 +1,35 @@
-export default function CafeDetailView(){
+import { StyleSheet, Text, View } from "react-native";
 
-  return(<></>)
+export default function CafeDetailView() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.cafePhoto}></View>
+      <View style={styles.confirmatContainer}></View>
+      <View style={styles.tabContainer}></View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1, alignItems: "center", backgroundColor: "pink" },
+  cafePhoto: {
+    height: 280,
+    width: "100%",
+    backgroundColor: "blue",
+  },
+  confirmatContainer: {
+    height: 75,
+    width: "100%",
+    alignItems: "center",
+    backgroundColor: "white",
+    borderBottomWidth: 1,
+    borderBottomColor: "#C3C3C3",
+  },
+  tabContainer: {
+    height: 54,
+    width: "100%",
+    borderBottomWidth: 1,
+    borderBottomColor: "#C3C3C3",
+    backgroundColor: "white",
+  },
+});
