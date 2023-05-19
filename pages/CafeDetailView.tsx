b38b887fab0527453226661ee8e3ecbd60efd843
limdumb <dlaruddls@nate.com>
2023-05-19 17:35:48 +0900

    [Feat] Cafe Detail Layout 구성

--- pages/CafeDetailView.tsx
+++ pages/CafeDetailView.tsx
@@ -1,19 +1,23 @@
-import { StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 
 export default function CafeDetailView() {
   return (
-    <View style={styles.container}>
+    <ScrollView contentContainerStyle={styles.container}>
       <View style={styles.cafePhoto}></View>
       <View style={styles.confirmatContainer}></View>
+      <View style={styles.cafeInfoTitleContainer}></View>
       <View style={styles.tabContainer}></View>
-    </View>
+      <View style={styles.adressInformation}></View>
+      <View style={styles.openTime}></View>
+      <View style={styles.phoneNumber}></View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, alignItems: "center", backgroundColor: "pink" },
+  container: { flex: 1, alignItems: "center" },
   cafePhoto: {
-    height: 280,
+    height: 240,
     width: "100%",
     backgroundColor: "blue",
   },
@@ -25,11 +29,39 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderBottomColor: "#C3C3C3",
   },
+  cafeInfoTitleContainer: {
+    width: "100%",
+    height: 74,
+    backgroundColor: "white",
+    borderBottomWidth: 1,
+    borderBottomColor: "#C3C3C3",
+  },
   tabContainer: {
-    height: 54,
+    height: 44,
     width: "100%",
     borderBottomWidth: 1,
     borderBottomColor: "#C3C3C3",
     backgroundColor: "white",
+  },
+  adressInformation: {
+    height: 59,
+    width: "100%",
+    backgroundColor: "white",
+    borderBottomWidth: 1,
+    borderBottomColor: "#C3C3C3",
+  },
+  openTime: {
+    width: "100%",
+    backgroundColor: "white",
+    borderBottomWidth: 1,
+    borderBottomColor: "#C3C3C3",
+    minHeight: 100,
+  },
+  phoneNumber: {
+    width: "100%",
+    backgroundColor: "white",
+    height: 73,
+    borderBottomWidth: 1,
+    borderBottomColor: "#C3C3C3",
   },
 });
