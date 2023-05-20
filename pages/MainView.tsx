baf119318c0cc3af9652978d58f7fb616ea227a9
limdumb <dlaruddls@nate.com>
2023-05-20 20:43:17 +0900

    [MISC] 충돌 해결

--- pages/MainView.tsx
+++ pages/MainView.tsx
@@ -1,25 +1,9 @@
-import { NavigationProp, useNavigation } from "@react-navigation/native";
-import { Button } from "react-native";
 import { StyleSheet, Text, View } from "react-native";
-import { RootStackParamList } from "../App";
-
-type CafeDetailNaviggationProps = NavigationProp<
-  RootStackParamList,
-  "MainView"
->;
 
 export default function MainView() {
-  const navigation = useNavigation<CafeDetailNaviggationProps>();
-
   return (
     <View style={styles.container}>
       <Text>하이용ㅋㅋ</Text>
-      <Button
-        title="하이"
-        onPress={() => {
-          navigation.navigate("SignUpView");
-        }}
-      />
     </View>
   );
 }
@@ -28,7 +12,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: "center"
   },
 });
