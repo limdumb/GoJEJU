6d93523e68be539050d2ebed299984a40e06ebc5
limdumb <dlaruddls@nate.com>
2023-05-24 15:35:00 +0900

    [MISC] 불필요한 코드 삭제

--- components/CommonInput.tsx
+++ components/CommonInput.tsx
@@ -6,12 +6,16 @@ interface InputProps {
   height: string;
   value: string;
   backgroundColor: string;
+  placeholder: string;
   changeFunc: React.Dispatch<React.SetStateAction<string>>;
 }
 
 const CustomInput = styled.TextInput<InputProps>`
   width: ${(props: InputProps) => props.width};
   height: ${(props: InputProps) => props.height};
+  border-radius: 10px;
+  padding-left: 10px;
+  padding-right: 10px;
 `;
 
 export default function CommonInput(props: InputProps) {
@@ -25,6 +29,7 @@ export default function CommonInput(props: InputProps) {
           props.changeFunc(text);
         }}
         backgroundColor={props.backgroundColor}
+        placeholder={props.placeholder}
       />
     </View>
   );
--- pages/CafeSearchView.tsx
+++ pages/CafeSearchView.tsx
@@ -1,22 +1,10 @@
-import { useState } from "react";
-import { Text } from "react-native";
 import { StyleSheet, View } from "react-native";
-import CommonInput from "../components/CommonInput";
 import Header from "../components/Header";
 
 export default function CafeSearchView() {
-  const [test, setTest] = useState("");
-
   return (
     <View style={styles.container}>
       <Header />
-      <CommonInput
-        backgroundColor="pink"
-        width="100px"
-        height="100px"
-        value={test}
-        changeFunc={setTest}
-      />
     </View>
   );
 }
