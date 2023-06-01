7928fa71d774be59a7673c9864e12a08aaba552f
limdumb <dlaruddls@nate.com>
2023-06-01 23:29:55 +0900

    [Desgin] Auth Login 조건부 랜더링 생성

--- components/AuthBox.tsx
+++ components/AuthBox.tsx
@@ -1,18 +1,27 @@
 import { Text, View } from "react-native";
 import styled from "styled-components/native";
+import CustomText from "./CustomText";
 
 interface AuthBoxProps {
   platform: "kakao" | "google";
 }
 
-const test = styled.View<AuthBoxProps>`
-  background-color: ${(props)=> props.platform};
+const AuthBoxContainer = styled.View<AuthBoxProps>`
+  background-color: ${(props) =>
+    props.platform === "kakao" ? "#FEE500" : "white"};
+  width: 80%;
+  height: 54px;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+  border-radius: 10px;
 `;
 
 export default function AuthBox(props: AuthBoxProps) {
   return (
-    <View>
-      <Text>하이</Text>
-    </View>
+    <AuthBoxContainer platform={props.platform}>
+      <CustomText children="KaKao Login" fontSize="18px" fontWeight="600"/>
+    </AuthBoxContainer>
   );
 }
