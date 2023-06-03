dd5ab0e20c7b073ac462113d42bbe41e65eee4dd
limdumb <dlaruddls@nate.com>
2023-06-03 15:08:17 +0900

    [Refact] Store Detail Home 컴포넌트 분리

--- components/DetailHomeView.tsx
+++ components/DetailHomeView.tsx
@@ -1,85 +1,73 @@
-import { useEffect, useState } from "react";
-import { Image, ScrollView, StyleSheet, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import CustomText from "./CustomText";
 import Icon from "react-native-vector-icons/FontAwesome5";
-import InstagramIcon from "react-native-vector-icons/FontAwesome";
+import StoreSchedule from "./StoreSchedule";
 import CallIcon from "react-native-vector-icons/MaterialIcons";
-import { StoreDetailType, getStoreDetail } from "../API/getStoreDetail";
-import CustomText from "../components/CustomText";
-import StatusToggle from "../components/StatusToggle";
-import StoreSchedule from "../components/StoreSchedule";
-import TabSwitcher from "../components/TabSwitcher";
-import { cautionText } from "../function/cautionText";
-import DetailHomeView from "../components/DetailHomeView";
+import InstagramIcon from "react-native-vector-icons/FontAwesome";
+import { StoreDetailType } from "../API/getStoreDetail";
 
-export default function StoreDetailView() {
-  const [isTabType, setIsTabType] = useState<"홈" | "리뷰">("홈");
-  const [storeDetails, setStoreDetails] = useState<StoreDetailType>({
-    id: 1,
-    images: [],
-    name: "",
-    storeStatus: "OPEN",
-    storeDescription: "",
-    jubunAddress: "",
-    roadAddress: "",
-    storeSchedules: [
-      {
-        day: "MONDAY",
-        start: "",
-        end: "",
-        lastOrder: "",
-        type: "OPEN",
-      },
-    ],
-    storePhoneNumber: "010-7702-9884",
-    sns: [{ type: "INSTARGRAM", url: "", nickName: "" }],
-  });
+interface DetailHomeViewProps {
+  storeDetails: StoreDetailType;
+}
 
-  useEffect(() => {
-    const fetchStoreDetail = async () => {
-      const response = await getStoreDetail({ id: storeDetails.id });
-      setStoreDetails(response);
-    };
-
-    fetchStoreDetail();
-  }, []);
-
+export default function DetailHomeView(props: DetailHomeViewProps) {
   return (
-    <View style={styles.container}>
-      <ScrollView style={styles.scrollView}>
-        <View style={styles.storePhotoContainer}>
-          {storeDetails.images.map((el) => {
+    <View>
+      <View style={styles.adressInformationContainer}>
+        <View style={styles.adressInfomation}>
+          <Icon name="map-marker-alt" size={25} style={styles.adressIcon} />
+          <CustomText
+            fontSize="15px"
+            children={props.storeDetails.jubunAddress}
+          />
+        </View>
+        <View style={styles.favoritContainer}>
+          <Icon name="star" size={20} style={styles.adressIcon} />
+          <CustomText children="즐겨찾기" />
+        </View>
+      </View>
+      <View style={styles.openTime}>
+        <View style={styles.openTitle}>
+          <Icon
+            name="clock"
+            color={"#80BFA0"}
+            size={20}
+            style={styles.clockIcon}
+          />
+          <CustomText
+            children="카페 영업시간"
+            fontWeight="600"
+            fontSize="16px"
+          />
+        </View>
+        <View style={styles.openSchedule}>
+          <StoreSchedule storeSchedules={props.storeDetails.storeSchedules} />
+        </View>
+      </View>
+      <View style={styles.contactContainer}>
+        <View style={styles.contactContents}>
+          <CallIcon name="call-end" size={30} color={"#4ECB71"} />
+          <CustomText
+            children={`전화번호 ${props.storeDetails.storePhoneNumber}`}
+            fontWeight={"bold"}
+            marginLft={"8px"}
+            fontSize={"15px"}
+          />
+        </View>
+        <View style={styles.contactContents}>
+          <InstagramIcon name="instagram" size={30} color={`#E4405F`} />
+          {props.storeDetails.sns.map((sns) => {
             return (
-              <Image key={el} style={styles.storePhoto} source={{ uri: el }} />
+              <CustomText
+                key={sns.nickName}
+                children={sns.nickName}
+                marginLft={"5px"}
+                fontWeight={"bold"}
+              />
             );
           })}
         </View>
-        <View style={styles.confirmatContainer}>
-          <View style={styles.onOffStatusContainer}>
-            <StatusToggle
-              storeStatus={storeDetails.storeStatus}
-              screen={"StoreDetailView"}
-            />
-          </View>
-          <View style={styles.cautionTextContainer}>
-            <CustomText
-              color="#9E9E9E"
-              fontSize="13px"
-              children={cautionText()}
-            />
-          </View>
-        </View>
-        <View style={styles.storeInfoTitleContainer}>
-          <CustomText
-            fontWeight="bold"
-            fontSize="16px"
-            children={storeDetails.storeDescription}
-          />
-        </View>
-        <View style={styles.tabContainer}>
-          <TabSwitcher />
-        </View>
-        <DetailHomeView storeDetails={storeDetails} />
-      </ScrollView>
+      </View>
     </View>
   );
 }
