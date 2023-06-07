549ec69c709b091b48affaef0de8a356f077849b
limdumb <dlaruddls@nate.com>
2023-06-07 20:18:28 +0900

    [Feat] Input 추가

--- pages/auth/UserNormalSignUpView.tsx
+++ pages/auth/UserNormalSignUpView.tsx
@@ -1,4 +1,57 @@
-export default function UserNormalSignUpView(){
+import { useState } from "react";
+import { View } from "react-native";
+import { StyleSheet } from "react-native";
+import AuthLogo from "../../components/AuthLogo";
+import CommonInput from "../../components/CommonInput";
 
-  return(<></>)
+export default function UserNormalSignUpView() {
+  const [emailId, setEmailId] = useState("");
+  const [passwordValue, setPasswordValue] = useState("")
+
+  return (
+    <View style={styles.container}>
+      <AuthLogo />
+      <View style={styles.inputContainer}>
+        <View>
+          <CommonInput
+            width={"100%"}
+            height={"44px"}
+            value={emailId}
+            backgroundColor={"#EEEEEE"}
+            placeholder={"이메일을 입력해 주세요"}
+            changeFunc={setEmailId}
+            label={"이메일"}
+            type={"text"}
+          />
+        </View>
+        <View style={styles.passwordContainer}>
+          <CommonInput
+            width={"100%"}
+            height={"44px"}
+            value={passwordValue}
+            backgroundColor={"#EEEEEE"}
+            placeholder={"비밀번호를 입력해 주세요"}
+            changeFunc={setPasswordValue}
+            label={"비밀번호"}
+            type={"password"}
+          />
+        </View>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+  },
+  inputContainer: {
+    height: 192,
+    width: "90%",
+  },
+  passwordContainer: {
+    marginTop: 20,
+  },
+});
