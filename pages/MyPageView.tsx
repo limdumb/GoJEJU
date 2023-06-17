ab77d2991f3fd5df6bd365b5710b9b166b2632dd
limdumb <dlaruddls@nate.com>
2023-06-17 17:53:57 +0900

    [Design] List Container 생성

--- pages/MyPageView.tsx
+++ pages/MyPageView.tsx
@@ -12,10 +12,16 @@ export default function MyPageView() {
         totalReviews={5}
         totalLikes={3}
       />
+      <View style={styles.line}/>
+      <View style={styles.menuList}>
+
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: { flex: 1, alignItems: "center", backgroundColor: "white" },
+  line:{width:"100%", borderBottomWidth:1, borderBottomColor:"#969090"},
+  menuList:{width:"100%",height:"60%", paddingTop:10}
 });
