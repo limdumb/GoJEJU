6ad5a987fa0d1eca0ad07ea373566f6d403a298d
limdumb <dlaruddls@nate.com>
2023-06-17 15:20:24 +0900

    [Feat] Email, Password Validation 함수 생성

---
+++ function/validation.ts
@@ -0,0 +1,12 @@
+export const emailValidation = (email: string): boolean => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  return emailRegex.test(email);
+};
+
+export const passwordValidation = (password: string): boolean => {
+  const passwordRegex =
+    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+
+  return passwordRegex.test(password);
+};
