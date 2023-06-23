3aefb9324d2ceed9827887b503737345c43aed56
limdumb <dlaruddls@nate.com>
2023-06-23 17:13:47 +0900

    [Feat] Store Suggest Data 추가

--- pages/StoreSuggestView.tsx
+++ pages/StoreSuggestView.tsx
@@ -1,13 +1,60 @@
-import { StyleSheet, View } from "react-native";
+import { useEffect, useState } from "react";
+import { ScrollView, StyleSheet, View } from "react-native";
+import { getSuggestList, SuggestDataType } from "../API/getSuggestList";
+import CustomText from "../components/CustomText";
 import Header from "../components/Header";
+import StoreBox from "../components/SearchView/StoreBox";
+import Spinner from "../components/Spinner";
 
 export default function StoreSuggestView() {
+  const [suggestData, setSuggestData] = useState<Array<SuggestDataType>>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchSuggestData = async () => {
+      setIsLoading(true);
+      const response = await getSuggestList();
+      setIsLoading(false);
+      if (!isLoading) {
+        setSuggestData(response);
+      }
+    };
+
+    fetchSuggestData();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Header />
-      <View>
-
-      </View>
+      <ScrollView>
+        <Header />
+        {isLoading ? (
+          <View style={styles.spinnerContainer}>
+            <Spinner />
+          </View>
+        ) : (
+          <>
+            <View style={styles.searchConuntContainer}>
+              <CustomText
+                children={`금주의 ${suggestData.length}개의 추천 카페`}
+                fontSize={"18px"}
+                fontWeight={"bold"}
+              />
+            </View>
+            <View style={styles.storeContainer}>
+              {suggestData.map((el) => {
+                return (
+                  <StoreBox
+                    imageUrl={el.imageUrl}
+                    name={el.name}
+                    storeDescription={el.storeDescription}
+                    rating={el.rating}
+                  />
+                );
+              })}
+            </View>
+          </>
+        )}
+      </ScrollView>
     </View>
   );
 }
@@ -16,7 +63,26 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    backgroundColor:"white"
+    backgroundColor: "white",
   },
-
+  titleContainer: {
+    width: "100%",
+    height: 60,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  searchConuntContainer: {
+    width: "100%",
+    height: 34,
+    backgroundColor: "#E8E8E8",
+    justifyContent: "center",
+    paddingLeft: 10,
+    marginBottom: 5,
+  },
+  storeContainer: { width: "100%", height: "100%" },
+  spinnerContainer: {
+    height: 600,
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
