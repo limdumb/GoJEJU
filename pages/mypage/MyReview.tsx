81354d25510bf8dddacc4b9d68f588d893ef1595
limdumb <dlaruddls@nate.com>
2023-06-26 12:03:24 +0900

    [Feat] 내 작성 리뷰 확인인 View 구현

--- pages/mypage/MyReview.tsx
+++ pages/mypage/MyReview.tsx
@@ -1,12 +1,41 @@
-import { StyleSheet, View } from "react-native";
+import { useEffect, useState } from "react";
+import { ScrollView, StyleSheet, View } from "react-native";
+import { getReviewList, ReviewType } from "../../API/getReviewList";
+import ReviewBox from "../../components/StoreDetailView/ReviewBox";
 
-export default function MyReview(){
+export default function MyReview() {
+  const [reviewData, setReviewData] = useState<Array<ReviewType>>([]);
+  const [pages, setPages] = useState(0);
 
-  return(<View>
-    {/* 뭐해야되는지 알지? 만들어 */}
-  </View>)
+  useEffect(() => {
+    const fetchReviewData = async () => {
+      const response = await getReviewList({ page: pages });
+      setReviewData(response.reviews);
+    };
+
+    fetchReviewData();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+        {reviewData.map((el) => {
+          return (
+            <ReviewBox
+              key={el.id}
+              id={el.id}
+              userName={el.userName}
+              userProfileImage={el.userProfileImage}
+              reviewImages={el.reviewImages}
+              reviewText={el.reviewText}
+            />
+          );
+        })}
+      </ScrollView>
+    </View>
+  );
 }
 
-const styles=StyleSheet.create({
-
-})
+const styles = StyleSheet.create({
+  container: { flex: 1, alignItems: "center", backgroundColor: "white" },
+});
