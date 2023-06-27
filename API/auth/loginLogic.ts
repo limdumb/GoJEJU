1b0adbde6aff4885eae28224ef9faf2d0a64499d
limdumb <dlaruddls@nate.com>
2023-06-27 11:38:26 +0900

    [Refact] Auth 로직 변경

--- API/auth/loginLogic.ts
+++ API/auth/loginLogic.ts
@@ -5,16 +5,25 @@ interface LoginPropsType {
   password: string;
 }
 
+export interface AuthResponseType {
+  accessToken: string;
+  refreshToken: string;
+  userId: number;
+  userType: "user" | "owner";
+}
+
 export default async function loginLogic(props: LoginPropsType) {
   const request = { email: props.email, password: props.password };
   try {
-    const response = {
+    const response:AuthResponseType = {
       accessToken: "berer",
       refreshToken: "berer",
       userId: 2,
       userType: "user",
     };
     await AsyncStorage.setItem("userId", `${response.userId}`);
+    await AsyncStorage.setItem("userType", response.userType);
+    await AsyncStorage.setItem("accessToken" , response.accessToken)
   } catch (err) {
     console.error(err);
     // 추후 공동 Error처리 진행 예정
