bde3dff4c18d69d021ee59b93019ad7a19b8e007
limdumb <dlaruddls@nate.com>
2023-06-28 14:10:43 +0900

    [Chore] React Native Checkbox 라이브러리 추가

---
+++ components/OwnerSettingView/ScheduleBox.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+import { View } from "react-native";
+
+export default function ScheduleBox(){
+
+  return(<View>
+
+  </View>)
+}
+
+const styles = StyleSheet.create({
+
+})
