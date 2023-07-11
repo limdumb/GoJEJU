5502d5a27b1b4aa34418bce32954874bee850a8e
limdumb <dlaruddls@nate.com>
2023-07-11 21:46:03 +0900

    [Feat] 검색결과 없음 컴포넌트 생성

--- pages/StoreSearchView.tsx
+++ pages/StoreSearchView.tsx
@@ -1,51 +1,49 @@
-import { useState } from 'react'
-import Header from '../components/Header'
-import CustomText from '../components/CustomText'
-import StoreBox from '../components/SearchView/StoreBox'
-import { Image, ScrollView, StyleSheet, View } from 'react-native'
-import SearchInput from '../components/SearchView/SearchInput'
-import getSearch, { type SearchDataType } from '../API/getSearch'
-import Spinner from '../components/Spinner'
-import { type NavigationProp, useNavigation } from '@react-navigation/native'
-import { type RootStackParamList } from '../App'
+import { useState } from "react";
+import Header from "../components/Header";
+import CustomText from "../components/CustomText";
+import StoreBox from "../components/SearchView/StoreBox";
+import { Image, ScrollView, StyleSheet, View } from "react-native";
+import SearchInput from "../components/SearchView/SearchInput";
+import getSearch, { type SearchDataType } from "../API/getSearch";
+import Spinner from "../components/Spinner";
+import { type NavigationProp, useNavigation } from "@react-navigation/native";
+import { type RootStackParamList } from "../App";
+import NotFoundImage from "../components/NotFoundImage";
 
 type StoreSearchNavigate = NavigationProp<
-RootStackParamList,
-'StoreSearchView'
->
+  RootStackParamList,
+  "StoreSearchView"
+>;
 
-export default function StoreSearchView () {
-  const navigate = useNavigation<StoreSearchNavigate>()
-  const [searchValue, setSearchValue] = useState('')
-  const [isSearchCompleted, setIsSearchCompleted] = useState(true)
-  const [isLoading, setIsLoading] = useState(false)
-  const [searchResults, setSearchResults] = useState<SearchDataType[]>([])
+export default function StoreSearchView() {
+  const navigate = useNavigation<StoreSearchNavigate>();
+  const [searchValue, setSearchValue] = useState("");
+  const [isSearchCompleted, setIsSearchCompleted] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [searchResults, setSearchResults] = useState<SearchDataType[]>([]);
 
   const fetchSearchResults = async () => {
-    setIsLoading(true)
+    setIsLoading(true);
     const response = await getSearch({
-      searchValue
-    })
-    setIsLoading(false)
+      searchValue,
+    });
+    setIsLoading(false);
     if (!isLoading) {
-      setSearchResults(response)
-      setIsSearchCompleted(false)
+      setSearchResults(response);
+      setIsSearchCompleted(false);
     }
-  }
+  };
 
   return (
     <View style={styles.container}>
       <ScrollView style={styles.scrollViewContainer}>
         <Header />
-        {!isSearchCompleted
-          ? (
-              isLoading
-                ? (
+        {!isSearchCompleted ? (
+          isLoading ? (
             <View style={styles.spinnerContainer}>
               <Spinner />
             </View>
-                  )
-                : (
+          ) : (
             <>
               <View style={styles.searchCompletedSection}>
                 <SearchInput
@@ -58,11 +56,10 @@ export default function StoreSearchView () {
               <View style={styles.searchConuntContainer}>
                 <CustomText
                   children={`${searchResults.length}개의 카페 검색결과`}
-                  fontSize={'18px'}
+                  fontSize={"18px"}
                 />
               </View>
-              {searchResults.length !== 0
-                ? (
+              {searchResults.length !== 0 ? (
                 <View style={styles.searchResultContainer}>
                   {searchResults.map((el) => {
                     return (
@@ -74,15 +71,13 @@ export default function StoreSearchView () {
                         storeDescription={el.storeDescription}
                         rating={el.rating}
                       />
-                    )
+                    );
                   })}
                 </View>
-                  )
-                : null}
+              ) : <NotFoundImage/>}
             </>
-                  )
-            )
-          : (
+          )
+        ) : (
           <>
             <View style={styles.searchInputSection}>
               <SearchInput
@@ -96,52 +91,52 @@ export default function StoreSearchView () {
               <Image
                 style={styles.jejuImageContainer}
                 source={{
-                  uri: 'https://blog.kakaocdn.net/dn/PU90c/btrfoSHCUh8/k7kGSgYM7tg8vpiwIglJy1/img.jpg'
+                  uri: "https://blog.kakaocdn.net/dn/PU90c/btrfoSHCUh8/k7kGSgYM7tg8vpiwIglJy1/img.jpg",
                 }}
               />
             </View>
           </>
-            )}
+        )}
       </ScrollView>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center'
+    backgroundColor: "white",
+    alignItems: "center",
   },
-  scrollViewContainer: { height: '100%' },
+  scrollViewContainer: { height: "100%" },
   searchInputSection: {
-    position: 'absolute',
-    alignItems: 'center',
+    position: "absolute",
+    alignItems: "center",
     top: 190,
     zIndex: 1000,
-    width: '100%'
+    width: "100%",
   },
   searchCompletedSection: {
-    width: '100%',
-    height: 100,
-    alignItems: 'center',
-    justifyContent: 'center'
+    width: "100%",
+    height: 80,
+    alignItems: "center",
+    justifyContent: "center",
   },
   searchConuntContainer: {
-    width: '100%',
+    width: "100%",
     height: 34,
-    backgroundColor: '#E8E8E8',
-    justifyContent: 'center',
-    paddingLeft: 10
+    backgroundColor: "#E8E8E8",
+    justifyContent: "center",
+    paddingLeft: 10,
   },
   searchResultContainer: {
-    width: '100%',
-    height: '100%'
+    width: "100%",
+    height: "100%",
   },
-  jejuImageContainer: { width: '100%', height: '208%' },
+  jejuImageContainer: { width: "100%", height: "208%" },
   spinnerContainer: {
     height: 600,
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
