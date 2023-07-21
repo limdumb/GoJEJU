4d905763490a08b01501508ce5013ca550a9b30b
limdumb <dlaruddls@nate.com>
2023-07-21 21:37:06 +0900

    [Refact] Favorit 요청 End Point 수정

--- API/favorit/addFavorit.ts
+++ API/favorit/addFavorit.ts
@@ -1,15 +1,12 @@
-import { baseInstance } from "../instance"
+import { baseInstance } from "../instance";
 
 export interface FavoritProps {
-  userId: number
+  userId: number;
 }
 
 export const addFavorit = async (props: FavoritProps) => {
-  const request = {userId: props.userId}
-  try{
-    const response = await baseInstance.post("", request)
-    return response.status
-  }catch(err){
-
-  }
-}
+  try {
+    const response = await baseInstance.post(`/api/favorit/${props.userId}`);
+    return response.status;
+  } catch (err) {}
+};
--- API/favorit/deleteFavorit.ts
+++ API/favorit/deleteFavorit.ts
@@ -2,9 +2,8 @@ import { FavoritProps } from "./addFavorit";
 import { baseInstance } from "../instance";
 
 export const deleteFavorit = async (props: FavoritProps) => {
-  const request = {userId: props.userId}
   try{
-    const response = await baseInstance.post("", request)
+    const response = await baseInstance.post(`/api/favorit/${props.userId}`)
     return response.status
   }catch(err){
 
