948f958d2f0cd8546406226e5b8742d7f13b32d6
limdumb <dlaruddls@nate.com>
2023-07-30 12:15:37 +0900

    [MISC] Store Type 수정

--- pages/StoreDetailView.tsx
+++ pages/StoreDetailView.tsx
@@ -34,7 +34,7 @@ interface StoreSchedulesType {
   type: "OPEN" | "CLOSED";
 }
 
-interface StoreDetailType {
+export interface StoreDetailType {
   id: number;
   images: string[];
   name: string;
@@ -64,7 +64,7 @@ interface ReviewResponseType {
 }
 
 export default function StoreDetailView({ route }: StoreDetailProps) {
-  const storeId = route.params.id;
+  const storeId = route.params.storeId;
   const userId = useUserId();
   const [reviewFilter, setReviewFilter] = useState<"최신순" | "추천순">(
     "최신순"
